5eed67f38baaa40ea0bd4ef3b3ff2729f35e1a76
Milos Stojkovic <iqoologic@gmail.com>
2024-11-18 00:06:00 +0100

    fixed wrong method call in Utils.isProjectRoot()

--- src/main/kotlin/rs/devlabs/gradle/project/info/Utils.kt
+++ src/main/kotlin/rs/devlabs/gradle/project/info/Utils.kt
@@ -5,15 +5,10 @@ import com.intellij.openapi.roots.ProjectRootManager
 import com.intellij.openapi.vfs.VirtualFile
 
 class Utils {
-    fun isProjectRoot(project: Project?, virtualFile: VirtualFile?): Boolean {// works
-        if (project == null || virtualFile == null) return false
-        val projectRoot = ProjectRootManager.getInstance(project).contentRoots.firstOrNull()
-        return projectRoot == virtualFile
-    }
-
     companion object {
-        fun isProjectRoot(project: Project, file: VirtualFile): Boolean {
-            return isProjectRoot(project, file)
+        fun isProjectRoot(project: Project, virtualFile: VirtualFile): Boolean {
+            val projectRoot = ProjectRootManager.getInstance(project).contentRoots.firstOrNull()
+            return projectRoot == virtualFile
         }
     }
 }
