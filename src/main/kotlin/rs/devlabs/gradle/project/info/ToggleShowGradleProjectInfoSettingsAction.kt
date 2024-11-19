003e7febce227cb2f6aaf7afe9daeb39a84cbc74
Milos Stojkovic <iqoologic@gmail.com>
2024-11-19 01:56:37 +0100

    fixed conflicts between Gradle and Maven project info plugins on adding action to project view gear options

--- src/main/kotlin/rs/devlabs/gradle/project/info/ToggleShowGradleProjectInfoSettingsAction.kt
+++ src/main/kotlin/rs/devlabs/gradle/project/info/ToggleShowGradleProjectInfoSettingsAction.kt
@@ -1,29 +1,27 @@
 package rs.devlabs.gradle.project.info
 
+import com.intellij.ide.projectView.ProjectView
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.ToggleAction
 import com.intellij.openapi.components.service
-import kotlinx.coroutines.Runnable
 
 class ToggleShowGradleProjectInfoSettingsAction : ToggleAction("Toggle Gradle Project Info") {
     private val settings = service<GradleProjectInfoSettings>()
-    private var onUpdateListener: Runnable = Runnable {}
 
-    override fun isSelected(p0: AnActionEvent): Boolean {
+    override fun isSelected(e: AnActionEvent): Boolean {
         return settings.enabled
     }
 
-    override fun setSelected(p0: AnActionEvent, enabled: Boolean) {
+    override fun setSelected(e: AnActionEvent, enabled: Boolean) {
         settings.enabled = enabled
-        this.onUpdateListener.run()
+        // Refresh project view directly here
+        e.project?.let { project ->
+            ProjectView.getInstance(project)?.refresh()
+        }
     }
 
     override fun getActionUpdateThread(): ActionUpdateThread {
         return ActionUpdateThread.BGT
-    }
-
-    fun setOnUpdateListener(listener: Runnable) {
-        onUpdateListener = listener
     }
 }
